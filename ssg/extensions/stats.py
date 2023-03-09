720127e6fcca697461fffee6abbce61984e9de74
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-09 14:12:33 +1030

    mistakenly tried to set global variable on global declaration
    from
    global start_time = time.time()
    to:
    global start_time
        start_time = time.time()

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -6,15 +6,17 @@ total_written = 0
 
 @hooks.register("start_build")
 def start_build():
-    global start_time = time.time()
+    global start_time
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
-    global total_written += 1
+    global total_written
+    total_written += 1
 
 @hooks.register("stats")
 def stats():
     final_time = time.time() - start_time
-    avergage = final_time / total_written if not 0
+    average = final_time / total_written
     report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
-    print(report.format(total_written, final_time, avergage))
+    print(report.format(total_written, final_time, average))
