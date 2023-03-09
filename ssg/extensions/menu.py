1935491963d72e8bb4637f9e578620d36e49d916
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-09 12:58:18 +1030

    fixed lambdas

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,7 +4,7 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: True if p is not isinstance(parsers.ResourceParser) else False
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in site_parsers.list(filter(valid)):
             if path.suffix.valid(parser.valid_file_ext()):
@@ -13,7 +13,7 @@ def collect_files(source, site_parsers):
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
-    menu_item = lambda name, ext: template.format(name, ext, name)
+    menu_item = lambda name, ext: template.format(name, ext, name.tilte())
     menu = ["\n".join(menu_item(path.stem, ext) for path in files)]
     return "<ul>\n{}<ul>\n{}".format(menu, html)
 
